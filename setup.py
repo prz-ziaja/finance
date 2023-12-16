811905a8f420a57ad8445197dc44238cab6643e9
Przemek <prz.ziaja@gmail.com>
2023-12-16 15:19:22 +0100

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name='MTN',
+      version='0.1',
+      description='Mellin Transform Network',
+      package_dir={"": "src"},
+      packages=find_packages(where="src"),
+      url="https://github.com/pypa/sampleproject",
+          author="Example Author",
+    author_email="author@example.com",
+     )
