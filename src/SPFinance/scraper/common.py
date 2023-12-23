b9e16a49a521ff82a4fa8a9ce880895a2b6e469c
Przemek <prz.ziaja@gmail.com>
2023-12-23 20:41:24 +0100

    add return object

--- src/SPFinance/scraper/common.py
+++ src/SPFinance/scraper/common.py
@@ -2,19 +2,26 @@ import pandas as pd
 import yfinance as yf
 import datetime as dt
 
-def get_stock(symbol:str, start_datetime: dt.datetime, end_datetime:dt.datetime, interval:str):
+def get_stock(symbol:str, start_datetime: dt.datetime, end_datetime:dt.datetime, interval:str = ""):
     """
     
     """
     symbol = symbol.upper()
-    data = yf.download(symbol, start_datetime, end_datetime, interval=interval)
+
+    if interval:
+        data = yf.download(symbol, start_datetime, end_datetime, interval=interval)
+    else:
+        data = yf.download(symbol, start_datetime, end_datetime)
+
     data_tz = data.tz_localize(yf.Ticker(symbol).info['timeZoneFullName'])
     d = data_tz.reset_index()
-    d['symbol'] = symbol
+
     if 'Date' in d.columns:
         d['observed_at'] = d.Date.apply(lambda x: x+dt.timedelta(hours=23,minutes=59))
     elif 'Datetime' in d.columns:
         d['observed_at'] = d.Datetime
+
+    d['symbol'] = symbol
     d['observed_at_utc'] = pd.Series([1 for _ in range(len(d.observed_at))],index=d.observed_at).tz_convert('UTC').reset_index()['observed_at']
     d['interval'] = 24*60*60
     d.rename(
@@ -28,3 +35,4 @@ def get_stock(symbol:str, start_datetime: dt.datetime, end_datetime:dt.datetime,
         },
         inplace=True
     )
+    return d
